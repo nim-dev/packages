3e0ab0058b0b3831eee7ee306ac2496d6ef77d01
Gabriel Huber <mail@gabrielhuber.at>
2023-03-07 03:53:14 +0100

    Add debug output

--- getmergebase.sh
+++ getmergebase.sh
@@ -8,6 +8,8 @@ targetBranch="$GITHUB_BASE_REF"
 cp packages.json packages.json.bak
 git fetch "$targetRepository" "$targetBranch:base"
 # Get packages.json at the branching point
+git status
+git merge-base HEAD base
 git checkout "$(git merge-base HEAD base)" packages.json
 mv packages.json packages_old.json
 mv packages.json.bak packages.json
